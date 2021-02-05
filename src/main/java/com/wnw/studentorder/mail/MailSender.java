c7f98b1765fe55c35cedfc8cad23d5ab5c4b3148
whisperworker <thewhisperworker@gmail.com>
2021-02-05 21:13:02 +0300

    Added package com.wnw.studentorder.mail Added MailSender

---
+++ src/main/java/com/wnw/studentorder/mail/MailSender.java
@@ -0,0 +1,9 @@
+package com.wnw.studentorder.mail;
+
+import com.wnw.studentorder.domain.StudentOrder;
+
+public class MailSender {
+    public void sendMail(StudentOrder so) {
+        System.out.println("Mail is sended");
+    }
+}
