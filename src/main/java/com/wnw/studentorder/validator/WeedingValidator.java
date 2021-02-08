fbd1ab47bc6b992c00e36e1f0934f1ae287e5b32
Александр <thewhisperworker@gmail.com>
2021-02-08 17:35:52 +0300

    Added get set hostName

--- src/main/java/com/wnw/studentorder/validator/WeedingValidator.java
+++ src/main/java/com/wnw/studentorder/validator/WeedingValidator.java
@@ -1,11 +1,19 @@
 package com.wnw.studentorder.validator;
 
 import com.wnw.studentorder.domain.AnswerWeeding;
-import com.wnw.studentorder.domain.StudentOrder;
+import com.wnw.studentorder.domain.weeding.StudentOrder;
 
 public class WeedingValidator {
 
-    public String hostName;
+    private String hostName;
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
 
     public AnswerWeeding checkWedding(StudentOrder so) {
         System.out.println("Wedding is checking:" + hostName);
