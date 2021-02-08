9a423b231b458c0ce56c06b73107351a25b1522a
Александр <thewhisperworker@gmail.com>
2021-02-08 17:29:17 +0300

    Added error processing

--- src/main/java/com/wnw/studentorder/validator/CityRegisterValidator.java
+++ src/main/java/com/wnw/studentorder/validator/CityRegisterValidator.java
@@ -1,16 +1,67 @@
 package com.wnw.studentorder.validator;
 
-import com.wnw.studentorder.domain.AnswerCityRegister;
-import com.wnw.studentorder.domain.StudentOrder;
+import com.wnw.studentorder.domain.register.AnswerCityRegister;
+import com.wnw.studentorder.domain.register.AnswerCityRegisterItem;
+import com.wnw.studentorder.domain.weeding.Child;
+import com.wnw.studentorder.domain.weeding.Person;
+import com.wnw.studentorder.exception.TransportException;
+import com.wnw.studentorder.validator.register.CityRegisterChecker;
+import com.wnw.studentorder.domain.register.CityRegisterResponse;
+import com.wnw.studentorder.domain.weeding.StudentOrder;
+import com.wnw.studentorder.exception.CityRegisterException;
+import com.wnw.studentorder.validator.register.FakeCityRegisterChecker;
+
+import java.util.List;
 
 public class CityRegisterValidator {
 
-    public String hostName;
+    public static final String IN_CODE = "NO_GRN";
 
+    private CityRegisterChecker personChecker;
+
+    public CityRegisterValidator(){
+        personChecker = new FakeCityRegisterChecker();
+    }
 
     public AnswerCityRegister checkCityRegister(StudentOrder so) {
-        System.out.println("City register is running:" + hostName);
-        AnswerCityRegister ans = new AnswerCityRegister();
+
+            AnswerCityRegister ans = new AnswerCityRegister();
+
+            ans.addItem(checkPerson(so.getHusband()));
+            ans.addItem(checkPerson(so.getWife()));
+
+            List<Child> children = so.getChildren();
+
+            for (Child child: children) {
+                ans.addItem(checkPerson(child));
+            }
+
         return ans;
+    }
+
+    private AnswerCityRegisterItem checkPerson(Person person) {
+
+        AnswerCityRegisterItem.CityStatus status = null;
+        AnswerCityRegisterItem.CityError error = null;
+
+        try {
+            CityRegisterResponse tmp = personChecker.checkPerson(person);
+            status = tmp.isExisting() ?
+                    AnswerCityRegisterItem.CityStatus.YES :
+                    AnswerCityRegisterItem.CityStatus.NO;
+        }
+        catch (CityRegisterException ex){
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(ex.getCode(), ex.getMessage());
+        } catch (TransportException ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
+        }
+
+        AnswerCityRegisterItem answer = new AnswerCityRegisterItem(status, person, error);
+
+        return answer;
     }
 }
