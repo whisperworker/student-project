4938ac10593d266a4c148b9dc1a1e263cfdd8647
Александр <thewhisperworker@gmail.com>
2021-02-08 17:35:34 +0300

    Added TransportException

---
+++ src/main/java/com/wnw/studentorder/exception/TransportException.java
@@ -0,0 +1,15 @@
+package com.wnw.studentorder.exception;
+
+public class TransportException extends Exception{
+
+    public TransportException() {
+    }
+
+    public TransportException(String message) {
+        super(message);
+    }
+
+    public TransportException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
