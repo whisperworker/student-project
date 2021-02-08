b7f0014fd73f8031a0ccd19d13892cff3fe404c3
Александр <thewhisperworker@gmail.com>
2021-02-08 17:26:40 +0300

    Added get, set methods

--- src/main/java/com/wnw/studentorder/validator/ChildrenValidator.java
+++ src/main/java/com/wnw/studentorder/validator/ChildrenValidator.java
@@ -1,11 +1,19 @@
 package com.wnw.studentorder.validator;
 
 import com.wnw.studentorder.domain.AnswerChildren;
-import com.wnw.studentorder.domain.StudentOrder;
+import com.wnw.studentorder.domain.weeding.StudentOrder;
 
 public class ChildrenValidator {
 
-    public String hostName;
+    private String hostName;
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
 
     public AnswerChildren checkChildren(StudentOrder so) {
         System.out.println("Children is checking:" + hostName);
